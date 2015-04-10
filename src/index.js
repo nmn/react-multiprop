855ec0afc146671fab39e395d506e96d7367d2c9
Naman Goel <naman34@gmail.com>
2015-04-10 18:21:13 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+import assign from 'react/lib/Object.assign'
+
+class MultiProp extends Component {
+  constructor(props){
+    super(props)
+    this.state = {index: 0}
+  }
+
+  componentDidMount(){
+    setTimeout(this.updateCount, this.props.interval)
+  }
+
+  updateCount(){
+    this.setState({index: (this.state.index + 1) % this.props.changingProps.length})
+  }
+
+  render(){
+
+    const Component = this.props.component
+
+    let props = assign({}, this.props, this.props.changingProps[this.state.index])
+    delete props.changingProps
+    delete props.interval
+    delete props.component
+
+    return (
+      <Component {...props} />
+    )
+  }
+}
+
+MultiProp.propTypes =
+  { changingProps: React.PropTypes.array.isRequired
+  , interval: React.PropTypes.number.isRequired
+  , component: React.PropTypes.component.isRequired
+  }
+
+MultiProp.displayName = 'MultiProp'
+
+export default MultiProp
